42d98039b9d6997cec058c61b103ccf9c74f08ce
IMRANISMAIL <imranismail@ubuntu.(none)>
2012-12-20 20:36:35 +1300

    First Commit

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+  def detail
+  end
+
+  def another
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demo::Application.config.secret_token = 'fd3185643aa07956076b7de8b0e3d9b282287b55f7425ff0ad369dab6f19280e36649c131345cd1f67eb38c362e6ea9c03c15d87f443f95d562127835dc0517d'
